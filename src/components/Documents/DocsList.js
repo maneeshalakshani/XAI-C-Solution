e83d980f069b3d2229381eed1f9e38cf3b022ec9
maneeshalakshani <maneeshalakshani9@gmail.com>
2023-11-07 22:21:01 +0530

    update: documents updated

--- src/components/Documents/DocsList.js
+++ src/components/Documents/DocsList.js
@@ -1,53 +1,69 @@
 import researchPaperDoc from '../../assets/files/research-paper.pdf'
+
 import researchPaperConfirmationDoc from '../../assets/files/confirmation.pdf'
 
+import pp1Doc from '../../assets/files/Proposal Presentation 1 - TMP_23_142.pdf'
+import pp2Doc from '../../assets/files/Proposal Presentation 2 - TMP_23_142 .pdf'
+
+import pp1Manee from '../../assets/files/Proposal Report_TMP 23-142_ IT20013950.pdf'
+import pp1Sashi from '../../assets/files/Proposal Report_TMP 23-142_IT20097660.pdf'
+import pp1Thila from '../../assets/files/Proposal Report_TMP 23-142_ IT20100698.pdf'
+import pp1Siri from '../../assets/files/Proposal Report_TMP 23-142_IT18161298.pdf'
+
+import ss1Manee from '../../assets/files/Project Status Document 1_IT20013950.pdf'
+import ss1Thila from '../../assets/files/Project Status Document 1_IT20100698.pdf'
+import ss1sashi from '../../assets/files/Project Status Document 1_IT20097660.pdf'
+import ss1Siri from '../../assets/files/Project Status Document 1_IT18161298.pdf'
+
+import frManee from '../../assets/files/Final Report_IT20013950.pdf'
+import frThila from '../../assets/files/Final Report_IT20100698.pdf'
+import frSiri from '../../assets/files/Final Report _IT18161298 .pdf'
+import frSashi from '../../assets/files/Final Report _IT20097660.pdf'
+
 export const ppDocs = [
-    { docTitle: "Document 1", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
-    { docTitle: "Document 2", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
-    { docTitle: "Document 3", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
-    { docTitle: "Document 4", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
-    { docTitle: "Document 5", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
-    { docTitle: "Document 6", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
-    { docTitle: "Document 7", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
-    { docTitle: "Document 8", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
+    // { docTitle: "Proposal Presentation", url: "Proposal Presentation 1 - TMP_23_142.pdf", paper: pp1Doc },
+    { docTitle: "Proposal Document - IT20013950", url: "Proposal Report_TMP 23-142_ IT20013950.pdf", paper: pp1Manee },
+    { docTitle: "Proposal Document - IT20097660", url: "Proposal Report_TMP 23-142_IT20097660.pdf", paper: pp1Sashi },
+    { docTitle: "Proposal Document - IT18161298", url: "Proposal Report_TMP 23-142_IT18161298.pdf", paper: pp1Siri },
+    { docTitle: "Proposal Document - IT20100698", url: "Proposal Report_TMP 23-142_ IT20100698.pdf", paper: pp1Thila },
 ];
 
 export const sd1 = [
-    { docTitle: "S Document 1", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
-    { docTitle: "S Document 2", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
-    { docTitle: "S Document 3", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
-    { docTitle: "S Document 4", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
+    { docTitle: "Status Document 1 - IT20013950", url: "Project Status Document 1_IT20013950.pdf", paper: ss1Manee },
+    { docTitle: "Status Document 1 - IT20097660", url: "Project Status Document 1_IT20097660.pdf", paper: ss1sashi },
+    { docTitle: "Status Document 1 - IT18161298", url: "Project Status Document 1_IT18161298.pdf", paper: ss1Siri },
+    { docTitle: "Status Document 1 - IT20100698", url: "Project Status Document 1_IT20100698.pdf", paper: ss1Thila },
 ];
 
 export const sd2 = [
-    { docTitle: "S Document 1", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
-    { docTitle: "S Document 2", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
-    { docTitle: "S Document 3", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
-    { docTitle: "S Document 4", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
+    { docTitle: "Status Document 2 - IT20013950", url: "Project Status Document 1_IT20013950.pdf", paper: ss1Manee },
+    { docTitle: "Status Document 3 - IT20097660", url: "Project Status Document 1_IT20097660.pdf", paper: ss1sashi },
+    { docTitle: "Status Document 4 - IT18161298", url: "Project Status Document 1_IT18161298.pdf", paper: ss1Siri },
+    { docTitle: "Status Document 5 - IT20100698", url: "Project Status Document 1_IT20100698.pdf", paper: ss1Thila },
 ];
 
 export const pp1 = [
-    { docTitle: "S Document 1", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
+    { docTitle: "Proposal Presentation 1", url: "Proposal Presentation 1 - TMP_23_142.pdf", paper: pp1Doc },
 ];
 
 export const pp2 = [
-    { docTitle: "S Document 1", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
+    { docTitle: "Proposal Presentation 1", url: "Proposal Presentation 2 - TMP_23_142 .pdf", paper: pp2Doc },
 ];
 
 export const researchPaper = [
-    { docTitle: "S Document 1", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
+    { docTitle: "Research Paper (Counterfactual Explanations for Commonly Used Text Classifiers focus on Review Classification)", url: "Counterfactual Explanations for Commonly Used Text Classifiers focus on Review Classification.pdf", paper: researchPaperDoc },
 ];
 
 export const finalReport = [
-    { docTitle: "S Document 1", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
-    { docTitle: "S Document 2", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
-    { docTitle: "S Document 3", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
-    { docTitle: "S Document 4", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
+    { docTitle: "Final Report - IT20013950", url: "Final Report_IT20013950.pdf", paper: frManee },
+    { docTitle: "Final Report - IT20097660", url: "Final Report_IT20013950.pdf", paper: frSashi },
+    { docTitle: "Final Report - IT18161298", url: "Final Report_IT20013950.pdf", paper: frSiri },
+    { docTitle: "Final Report - IT20100698", url: "Final Report_IT20013950.pdf", paper: frThila },
 ];
 
 export const projectRegistrationDocs = [
-    { docTitle: "Conference Acceptance Confirmation", url: "Confirmation.pdf", paper: researchPaperConfirmationDoc },
-    { docTitle: "S Document 2", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
-    { docTitle: "S Document 3", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
-    { docTitle: "S Document 4", url: "srjt,hs/ahth/ahae", paper: researchPaperDoc },
+    { docTitle: "Conference Acceptance - Confirmation", url: "Confirmation.pdf", paper: researchPaperConfirmationDoc },
+    // { docTitle: "Conference Acceptance Confirmation", url: "Confirmation.pdf", paper: researchPaperConfirmationDoc },
+    // { docTitle: "Conference Acceptance Confirmation", url: "Confirmation.pdf", paper: researchPaperConfirmationDoc },
+    // { docTitle: "Conference Acceptance Confirmation", url: "Confirmation.pdf", paper: researchPaperConfirmationDoc },
 ];
